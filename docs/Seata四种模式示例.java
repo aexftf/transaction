50119b2866da10c3ed4ca453a4da724ce4615fad
Administrator <446729313@qq.com>
2026-02-16 18:26:10 +0800

    1

--- docs/Seata四种模式示例.java
+++ docs/Seata四种模式示例.java
@@ -275,7 +275,7 @@ class SAGA_Mode_Example {
         // 补偿步骤2：退票
         refundTicket(userId, flightNo);
 
-        // 补偿步骤1：退款1
+        // 补偿步骤1：退款
         refundPayment(userId, 1000);
     }
 
